5dca039b691a207d87a465a892ed3286ce6ceb19
Sébastien Gross <sebchezwamorg>
2020-03-05 02:30:05 +0100

    Make better unit test to cover 100%.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cwformatter_test.go
+++ cwformatter_test.go
@@ -2,8 +2,10 @@ package cwformatter
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
+	"github.com/logrusorgru/aurora"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -11,13 +13,18 @@ type runFunc func(...interface{})
 
 var l = log.New()
 
-func runTest(t *testing.T, name string, fct runFunc, input, expect string) {
-	b := &bytes.Buffer{}
-	l.Out = b
+func initLogger() *log.Logger {
 	f := NewFormatter()
 	f.Timeformat = ""
 	f.FieldsColumn = 30
 	l.Formatter = f
+
+	return l
+}
+
+func doLog(l *log.Logger, fct runFunc, input string) string {
+	b := &bytes.Buffer{}
+	l.Out = b
 
 	fct(input)
 
@@ -26,144 +33,139 @@ func runTest(t *testing.T, name string, fct runFunc, input, expect string) {
 	if lbs > 0 {
 		lbs--
 	}
-	if bs[:lbs] != expect {
-		t.Errorf("Test %s failed: -%s- instead of -%s-", name, bs[:lbs], expect)
+	return bs[:lbs]
+}
+
+func TestLevels(t *testing.T) {
+	l := initLogger()
+	tests := []struct {
+		n    string
+		f    runFunc
+		in   string
+		want string
+	}{
+		{"Trace", l.Trace, "Message", ""},
+		{"Debug", l.Debug, "Message", ""},
+		{"Info", l.Info, "Message", "Message"},
+		{"Warn", l.Warn, "Message", "Message"},
+		{"Error", l.Error, "Message", "Message"},
+		//{"Fatal", l.Fatal, "Message", "Message"},
+		//{"Panic", l.Panic, "Message", "Message"},
+
+		{"Error with fields", l.WithFields(log.Fields{"f1": "v1"}).Error,
+			"Some log", `Some log                      | f1="v1"`},
+
+		{"Start command", l.WithFields(log.Fields{"COMMAND_START": "ls -al /"}).Info,
+			"", `Running ls -al /`},
+
+		{"Success command", l.WithFields(log.Fields{"COMMAND_RESULT": 0}).Info,
+			"", ` ==> OK`},
+		{"Failed command", l.WithFields(log.Fields{"COMMAND_RESULT": 1}).Error,
+			"", ` ==> Failed (exit code 1)`},
+	}
+	for _, test := range tests {
+		t.Run(test.n, func(t *testing.T) {
+			got := doLog(l, test.f, test.in)
+			if got != test.want {
+				t.Errorf("got %q want %q", got, test.want)
+			}
+		})
 	}
 }
 
-func TestSimpleLog(t *testing.T) {
+func TestColors(t *testing.T) {
+	f := NewFormatter()
+	tests := []struct {
+		n string
+		l log.Level
+		c aurora.Color
+	}{
+		{"Panic", log.PanicLevel, f.PanicColor},
+		{"Fatal", log.FatalLevel, f.FatalColor},
+		{"Error", log.ErrorLevel, f.ErrorColor},
+		{"Warn", log.WarnLevel, f.WarnColor},
+		{"Info", log.InfoLevel, f.InfoColor},
+		{"Debug", log.DebugLevel, f.DebugColor},
+		{"Trace", log.TraceLevel, f.TraceColor},
+	}
+	for _, test := range tests {
+		t.Run(test.n, func(t *testing.T) {
+			got := f.color(test.l)
+			if got != test.c {
+				t.Errorf("got %q want %q", got, test.c)
+			}
+		})
+	}
+}
 
-	runTest(t, "Simple Error", l.Error, "Some log", "Some log")
-	runTest(t, "Simple Warn", l.Warn, "Some log", "Some log")
-	runTest(t, "Simple Info", l.Info, "Some log", "Some log")
-	runTest(t, "Simple Debug", l.Debug, "Some log", "")
-	runTest(t, "Simple Trace", l.Trace, "Some log", "")
-
-	runTest(t, "Error with fields", l.WithFields(log.Fields{
-		"f1": "v1",
-		//		"f1": "v1",
-	}).Error, "Some log",
-		`Some log                      | f1="v1"`)
-
-	// runTest(t, "Start command and field", l.WithFields(log.Fields{
-	// 	"COMMAND_START": "ls -al /inexistant",
-	// 	"event":         "event",
-	// }).Info, "", `Running ls -al /inexistant event="event"`)
-
-	runTest(t, "Start command", l.WithFields(log.Fields{
-		"COMMAND_START": "ls -al /",
-	}).Info, "", `Running ls -al /`)
-
-	runTest(t, "Success command", l.WithFields(log.Fields{
-		"COMMAND_RESULT": 0,
-	}).Info, "", ` ==> OK`)
-
-	runTest(t, "Failed command", l.WithFields(log.Fields{
-		"COMMAND_RESULT": 1,
-	}).Info, "", ` ==> Failed (exit code 1)`)
-
+func TestTerminal(t *testing.T) {
+	f := NewFormatter()
+	f.FieldsColumn = 30
+	l.Out = os.Stderr
+	l.Formatter = f
+	l.Warn("Foo")
 }
 
 func TestHooks(t *testing.T) {
-	b := &bytes.Buffer{}
-	l.Out = b
 	f := NewFormatter()
 	f.Timeformat = ""
 	f.FieldsColumn = 30
 	l.Formatter = f
 
-	f.DeleteHook("COMMAND_START")
-	l.WithFields(log.Fields{"COMMAND_START": "ls"}).Info("Message")
-	bs := b.String()
-	lbs := len(bs)
-	if lbs > 0 {
-		lbs--
-	}
-	expect := `Message                       | COMMAND_START="ls"`
-	if bs[:lbs] != expect {
-		t.Errorf("Test failed: -%s- instead of -%s-", bs[:lbs], expect)
-	}
-	b.Reset()
+	tests := []struct {
+		n    string
+		f    runFunc
+		in   string
+		want string
+		pre  func()
+	}{
+		{"Without COMMAND_START", l.WithFields(log.Fields{"COMMAND_START": "ls"}).Info,
+			"Message", `Message                       | COMMAND_START="ls"`,
+			func() { f.DeleteHook("COMMAND_START") }},
 
-	f.AddHook("COMMAND_START", commandStart)
-	l.WithFields(log.Fields{"COMMAND_START": "ls"}).Info("Message")
-	bs = b.String()
-	lbs = len(bs)
-	if lbs > 0 {
-		lbs--
-	}
-	expect = "Message                       | Running ls"
-	if bs[:lbs] != expect {
-		t.Errorf("Test failed: -%s- instead of -%s-", bs[:lbs], expect)
+		{"Without COMMAND_START", l.WithFields(log.Fields{"COMMAND_START": "ls"}).Info,
+			"", `COMMAND_START="ls"`,
+			func() { f.DeleteHook("COMMAND_START") }},
+
+		{"With COMMAND_START", l.WithFields(log.Fields{"COMMAND_START": "ls"}).Info,
+			"Message", `Message                       | Running ls`,
+			func() { f.AddHook("COMMAND_START", commandStart) }},
+
+		{"With COMMAND_START", l.WithFields(log.Fields{"COMMAND_START": "ls"}).Info,
+			"", `Running ls`,
+			func() { f.AddHook("COMMAND_START", commandStart) }},
 	}
 
-}
-
-func BenchmarkSimple(b *testing.B) {
-	// b := &bytes.Buffer{}
-	// l.Out = b
-	f := NewFormatter()
-	f.Timeformat = ""
-	f.FieldsColumn = 30
-	l.Formatter = f
-	for i := 0; i < b.N; i++ {
-		l.Info("Message")
+	for _, test := range tests {
+		t.Run(test.n, func(t *testing.T) {
+			test.pre()
+			got := doLog(l, test.f, test.in)
+			if got != test.want {
+				t.Errorf("got %q want %q", got, test.want)
+			}
+		})
 	}
 }
 
-func BenchmarkFields(b *testing.B) {
-	// b := &bytes.Buffer{}
-	// l.Out = b
-	f := NewFormatter()
-	f.Timeformat = ""
-	f.FieldsColumn = 30
-	l.Formatter = f
-	for i := 0; i < b.N; i++ {
-		l.WithFields(log.Fields{
-			"f1": "v1",
-			"f2": "v2",
-		}).Info("Message")
+func BenchmarkLevels(b *testing.B) {
+	l := initLogger()
+	tests := []struct {
+		n string
+		f runFunc
+	}{
+		{"Trace", l.Trace},
+		{"Error", l.Error},
+		{"Fields Trace", l.WithFields(log.Fields{"f1": "v1", "f2": "v2"}).Trace},
+		{"Fields Error", l.WithFields(log.Fields{"f1": "v1", "f2": "v2"}).Error},
+		{"CMD_START", l.WithFields(log.Fields{"COMMAND_START": "ls"}).Info},
+		{"CMD_RES OK", l.WithFields(log.Fields{"COMMAND_RESULT": 0}).Info},
+		{"CMD_RES ERR", l.WithFields(log.Fields{"COMMAND_RESULT": 1}).Error},
 	}
-}
-
-func BenchmarkCommandStart(b *testing.B) {
-	// b := &bytes.Buffer{}
-	// l.Out = b
-	f := NewFormatter()
-	f.Timeformat = ""
-	f.FieldsColumn = 30
-	l.Formatter = f
-	for i := 0; i < b.N; i++ {
-		l.WithFields(log.Fields{
-			"COMMAND_START": "ls",
-		}).Info("Message")
-	}
-}
-
-func BenchmarkCommandResultOK(b *testing.B) {
-	// b := &bytes.Buffer{}
-	// l.Out = b
-	f := NewFormatter()
-	f.Timeformat = ""
-	f.FieldsColumn = 30
-	l.Formatter = f
-	for i := 0; i < b.N; i++ {
-		l.WithFields(log.Fields{
-			"COMMAND_RESULT": 0,
-		}).Info("Message")
-	}
-}
-
-func BenchmarkCommandResultFail(b *testing.B) {
-	// b := &bytes.Buffer{}
-	// l.Out = b
-	f := NewFormatter()
-	f.Timeformat = ""
-	f.FieldsColumn = 30
-	l.Formatter = f
-	for i := 0; i < b.N; i++ {
-		l.WithFields(log.Fields{
-			"COMMAND_RESULT": 1,
-		}).Info("Message")
+	for _, test := range tests {
+		b.Run(test.n, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				doLog(l, test.f, "Message")
+			}
+		})
 	}
 }
